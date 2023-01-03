7e6adee5d21b97757a92cfdfe0ec1a70c9c0b27b
Red Ochsenbein <red@redochsenbein.ch>
2023-01-03 19:26:16 +0100

    Add filter module

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Case from './components/Case'
+import Filter12db from './modules/Filter12db'
 import Gain from './modules/Gain'
 import SingleOscillator from './modules/SingleOscillator'
 
@@ -8,6 +9,8 @@ const App: React.FC = () => {
   return (
     <Case>
       <SingleOscillator />
+      <SingleOscillator />
+      <Filter12db />
       <Gain />
     </Case>
   )
---
+++ src/modules/Filter12db.tsx
@@ -0,0 +1,64 @@
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react'
+
+import { CaseContext } from '../components/Case'
+import Knob from '../components/Knob'
+import Module from '../components/Module'
+import OutputSelector from '../components/OutputSelector'
+
+const Filter12db: React.FC = () => {
+  const filterNode = useRef<BiquadFilterNode>()
+
+  const [currentFrequency, setCurrentFrequency] = useState(2000)
+  const [currentQ, setCurrentQ] = useState(0)
+
+  const { audioCtx, registerModule } = useContext(CaseContext)
+
+  useEffect(() => {
+    // initialize the gain module
+    if (!audioCtx) {
+      return
+    }
+
+    filterNode.current = audioCtx.createBiquadFilter()
+    filterNode.current.frequency.setValueAtTime(0, audioCtx.currentTime)
+
+    registerModule({ moduleName: 'filter 12dB', inputs: { main: filterNode.current } })
+  }, [audioCtx, registerModule])
+
+  useEffect(() => {
+    // set the current frequency
+    if (!audioCtx || !filterNode.current) {
+      return
+    }
+
+    filterNode.current.frequency.setValueAtTime(currentFrequency, audioCtx.currentTime)
+  }, [audioCtx, currentFrequency])
+
+  useEffect(() => {
+    // set the current Q
+    if (!audioCtx || !filterNode.current) {
+      return
+    }
+
+    filterNode.current.Q.setValueAtTime(currentQ, audioCtx.currentTime)
+  }, [audioCtx, currentQ])
+
+  const handleFilterChange = useCallback((v: number) => setCurrentFrequency(v), [])
+  const handleQChange = useCallback((v: number) => setCurrentQ(v), [])
+
+  return (
+    <Module title="12dB Filter">
+      <div>
+        <Knob label="Freq." min={20} max={10000} step={20} value={currentFrequency} onChange={handleFilterChange} />
+      </div>
+      <div>
+        <Knob label="Q" min={0} max={30} step={0.5} value={currentQ} onChange={handleQChange} />
+      </div>
+      <div>
+        <OutputSelector audioNode={filterNode.current} moduleName="filter 12dB" />
+      </div>
+    </Module>
+  )
+}
+
+export default Filter12db
