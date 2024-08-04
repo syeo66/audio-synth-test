4c4c3e393deb231c4b296af3a5647fbf2b15bc60
Red Ochsenbein <red@redochsenbein.ch>
2024-08-04 21:56:27 +0200

    Add LFO module skeleton.

---
+++ src/modules/LFO.tsx
@@ -0,0 +1,121 @@
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react'
+
+import { CaseContext } from '../components/Case'
+import Knob from '../components/Knob'
+import Module from '../components/Module'
+import ModuleFooter from '../components/ModuleFooter'
+import ModuleSection from '../components/ModuleSection'
+import NodeOutputSelector from '../components/NodeOutputSelector'
+import PushButton from '../components/PushButton'
+import Sawtooth from '../icons/Sawtooth'
+import Sine from '../icons/Sine'
+import Square from '../icons/Square'
+import Triangle from '../icons/Triangle'
+
+const LFO: React.FC = () => {
+  const [oscillatorType, setOscillatorType] = useState<OscillatorType>('sine')
+  const [currentFrequency, setCurrentFrequency] = useState(1)
+  const [currentGain, setCurrentGain] = useState(1)
+
+  const oscillator = useRef<OscillatorNode>()
+  const gainNode = useRef<GainNode>()
+
+  const { audioCtx, registerModule } = useContext(CaseContext)
+
+  useEffect(() => {
+    if (!audioCtx) {
+      return
+    }
+
+    oscillator.current = audioCtx.createOscillator()
+    gainNode.current = audioCtx.createGain()
+    oscillator.current.connect(gainNode.current)
+    oscillator.current.start()
+    oscillator.current.frequency.setValueAtTime(1, audioCtx.currentTime)
+    gainNode.current.gain.setValueAtTime(1, audioCtx.currentTime)
+
+    registerModule({
+      moduleName: 'lfo',
+      inputs: {},
+    })
+
+    return () => {
+      oscillator.current?.stop()
+      oscillator.current?.disconnect()
+      gainNode.current?.disconnect()
+    }
+  }, [audioCtx, registerModule])
+
+  useEffect(() => {
+    if (!audioCtx) {
+      return
+    }
+    oscillator.current?.frequency.setValueAtTime(currentFrequency, audioCtx.currentTime)
+  }, [audioCtx, currentFrequency])
+
+  useEffect(() => {
+    if (!audioCtx) {
+      return
+    }
+    gainNode.current?.gain.setValueAtTime(currentGain, audioCtx.currentTime)
+  }, [audioCtx, currentGain])
+
+  const handleFrequencyChange = useCallback((v: number) => setCurrentFrequency(v), [])
+  const handleGainChange = useCallback((v: number) => setCurrentGain(v), [])
+  const handleSelect = useCallback((oscType: OscillatorType) => {
+    if (!oscillator.current) {
+      return
+    }
+    oscillator.current.type = oscType
+    setOscillatorType(oscType)
+  }, [])
+
+  const handleSelectSine = useCallback(() => handleSelect('sine'), [handleSelect])
+  const handleSelectTriangle = useCallback(() => handleSelect('triangle'), [handleSelect])
+  const handleSelectSquare = useCallback(() => handleSelect('square'), [handleSelect])
+  const handleSelectSawtooth = useCallback(() => handleSelect('sawtooth'), [handleSelect])
+
+  return (
+    <Module title="LFO">
+      <ModuleSection>
+        <PushButton onClick={handleSelectSine} active={oscillatorType === 'sine'}>
+          <Sine />
+        </PushButton>
+
+        <PushButton onClick={handleSelectTriangle} active={oscillatorType === 'triangle'}>
+          <Triangle />
+        </PushButton>
+      </ModuleSection>
+
+      <ModuleSection>
+        <PushButton onClick={handleSelectSquare} active={oscillatorType === 'square'}>
+          <Square />
+        </PushButton>
+
+        <PushButton onClick={handleSelectSawtooth} active={oscillatorType === 'sawtooth'}>
+          <Sawtooth />
+        </PushButton>
+      </ModuleSection>
+
+      <ModuleSection>
+        <Knob
+          label="Freq."
+          min={0.1}
+          max={20}
+          step={1000.1}
+          value={currentFrequency}
+          onChange={handleFrequencyChange}
+        />
+      </ModuleSection>
+      <ModuleSection>
+        <Knob label="Gain" min={0} max={100} step={0.01} value={currentGain} onChange={handleGainChange} />
+      </ModuleSection>
+
+      <ModuleFooter>
+        <NodeOutputSelector audioNode={gainNode.current} moduleName="lfo" />
+      </ModuleFooter>
+    </Module>
+  )
+}
+
+export default LFO
